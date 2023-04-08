1d753944d162aa43266f6936211694259b7add2c
DIDI <jisoodidi86@gmail.com>
2023-04-08 11:20:06 +0900

    layout:main레이아웃

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <main>
+      <button>
+        <Link to="/signin">로그인하고 todo만들러가기</Link>
+      </button>
+    </main>
+  );
 };
 
 export default Main;
