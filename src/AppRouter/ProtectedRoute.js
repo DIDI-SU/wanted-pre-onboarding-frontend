62e28fb89695cdbfeeca6d2a6e3800e76aa3f36a
DIDI <jisoodidi86@gmail.com>
2023-04-18 23:22:32 +0900

    add:경로수정

--- src/AppRouter/ProtectedRoute.js
+++ src/AppRouter/ProtectedRoute.js
@@ -1,10 +1,4 @@
-import {
-  Routes,
-  Route,
-  NavLink,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const TOKEN = JSON.parse(localStorage.getItem("TOKEN"));
